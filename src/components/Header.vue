b6a74976c92468e352ca82904f3f833798257d8e
hugo torres <hugo.torres@morningstar.com>
2022-10-26 09:44:00 -0500

    added image

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -38,8 +38,7 @@
           <li>
             <a
               href="/equilibrio/inicio"
-              class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-              aria-current="page"
+              class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >Equilibrio Quimico</a>
           </li>
           <li>
--- src/pages/equilibrio/[equilibrio].vue
+++ src/pages/equilibrio/[equilibrio].vue
@@ -1,17 +1,25 @@
-<script setup lang="ts">
-const props = defineProps<{ section: string }>()
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <Header />
-    <div i-carbon-chemistry text-4xl inline-block />
-    <p>
-      {{ props.section }}
-    </p>
-    <p text-sm op50>
-      href="/equilibrio/inicio"
-    </p>
-    <div />
+    <div class="grid grid-cols-2 gap-4">
+      <div class="text-left">
+        <p>
+          El equilibrio químico es el estado en el que las actividades químicas
+          o las concentraciones de los reactivos y los productos no tienen
+          ningún cambio neto. Normalmente, este sería el estado que se produce
+          cuando una reacción reversible evoluciona hacia adelante en la misma
+          proporción que su reacción inversa. La velocidad de reacción de las
+          reacciones directa e inversa por lo general no son cero, pero, si
+          ambas son iguales, no hay cambios netos en cualquiera de las
+          concentraciones de los reactivos o productos. Este proceso se denomina
+          equilibrio dinámico
+        </p>
+      </div>
+      <div>
+        <img src="./assets/images/reaccion-quimica.jpeg" alt="">
+      </div>
+    </div>
   </div>
 </template>
