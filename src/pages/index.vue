defb2720144968f376c8d11959d96bf0a609b177
hugo torres <hugo.torres@morningstar.com>
2022-10-26 09:05:42 -0500

    navigation

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,13 +3,7 @@
     <Header />
     <div
       class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 text-center"
-    >
-      <img
-        src="reaccion-quimica.jpeg"
-        alt="reaccion quimica"
-        class="mx-auto max-w-lg h-auto"
-      >
-    </div>
+    />
   </div>
 </template>
 
