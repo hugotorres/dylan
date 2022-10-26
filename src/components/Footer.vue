3ece302ceaa1c55c694831bb1650d411067c1bb4
hugo torres <hugo.torres@morningstar.com>
2022-10-26 10:51:40 -0500

    with bibliografia

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,34 @@
 <template>
-  <footer
-    class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800"
-  >
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">©Dylan Gonzalez
-    </span>
-    <ul
-      class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+  <div>
+    <div p-6>
+      <h4>Bibliografia:</h4>
+
+      <div p-6>
+        <p>
+          <a href="https://es.wikipedia.org/wiki/Equilibrio_qu%C3%ADmico">https://es.wikipedia.org/wiki/Equilibrio_qu%C3%ADmico</a>
+        </p>
+        <p>
+          <a href="https://es.wikipedia.org/wiki/Constante_de_equilibrio">https://es.wikipedia.org/wiki/Constante_de_equilibrio</a>
+        </p>
+        <p>
+          <a
+            href="https://alquimiajuanp.wordpress.com/tipos-de-equilibrio-quimico/"
+          >https://alquimiajuanp.wordpress.com/tipos-de-equilibrio-quimico/</a>
+        </p>
+      </div>
+    </div>
+    <footer
+      class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800"
     >
-      <li>
-        <a href="https://www.colegiocanapro.edu.co" class="hover:underline">colegio Canapro</a>
-      </li>
-    </ul>
-  </footer>
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">©Dylan Gonzalez
+      </span>
+      <ul
+        class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+      >
+        <li>
+          <a href="https://www.colegiocanapro.edu.co" class="hover:underline">colegio Canapro</a>
+        </li>
+      </ul>
+    </footer>
+  </div>
 </template>
